51a3ee5d07ada5c37b3a2d9629f6e89cc8d08471
Uyonoh <turakiuyonoh@gmail.com>
2025-04-03 18:54:45 +0100

    fix question export eror

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,33 +1,38 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-          function Question({ question, selectedAnswer, onSelect, onSubmit }) {
-            const allAnswers = [...question.incorrect_answers, question.correct_answer]
-              .sort(() => Math.random() - 0.5);
-            
-            return (
-              <div className="question-container">
-                <h2 dangerouslySetInnerHTML={{ __html: question.question }} />
-                
-                <div className="answers-grid">
-                  {allAnswers.map((answer, index) => (
-                    <button
-                      key={index}
-                      className={`answer-button ${selectedAnswer === answer ? 'selected' : ''}`}
-                      onClick={() => onSelect(answer)}
-                      dangerouslySetInnerHTML={{ __html: answer }}
-                    />
-                  ))}
-                </div>
-                
-                <button
-                  className="submit-button"
-                  onClick={() => onSubmit(selectedAnswer)}
-                  disabled={!selectedAnswer}
-                >
-                  Submit
-                </button>
-              </div>
-            );
-          }
+export default function Question({ question, selectedAnswer, onSelect, onSubmit }) {
+  const [shuffledAnswers, setShuffledAnswers] = useState([]);
 
-          export default Question;
+  // Shuffle only once when question changes
+  useEffect(() => {
+    if (question) {
+      const allAnswers = [...question.incorrect_answers, question.correct_answer];
+      setShuffledAnswers(allAnswers.sort(() => Math.random() - 0.5));
+    }
+  }, [question]); // Only re-run when question changes
+
+  return (
+    <div className="question-container">
+      <h2 dangerouslySetInnerHTML={{ __html: question.question }} />
+      
+      <div className="answers-grid">
+        {shuffledAnswers.map((answer, index) => (
+          <button
+            key={index}
+            className={`answer-button ${selectedAnswer === answer ? 'selected' : ''}`}
+            onClick={() => onSelect(answer)}
+            dangerouslySetInnerHTML={{ __html: answer }}
+          />
+        ))}
+      </div>
+      
+      <button
+        className="submit-button"
+        onClick={() => onSubmit(selectedAnswer)}
+        disabled={!selectedAnswer}
+      >
+        Submit
+      </button>
+    </div>
+  );
+}
